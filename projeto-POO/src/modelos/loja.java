7897a5e5038b2809bdc9865eef8948696814e868
KaiqueSousa <kaique.una@gmail.com>
2018-01-19 18:21:32 -0200

    Adicionando a class de Modelo loja

---
+++ projeto-POO/src/modelos/loja.java
@@ -0,0 +1,81 @@
+
+package modelos;
+
+import java.util.Objects;
+
+public class loja {
+    
+    private String nome;
+    private String codigo;
+    private String endereco;
+
+    public loja(String nome, String codigo, String endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.nome);
+        hash = 37 * hash + Objects.hashCode(this.codigo);
+        hash = 37 * hash + Objects.hashCode(this.endereco);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final loja other = (loja) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.codigo, other.codigo)) {
+            return false;
+        }
+        if (!Objects.equals(this.endereco, other.endereco)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "loja{" + "nome=" + nome + ", codigo=" + codigo + ", endereco=" + endereco + '}';
+    }
+    
+    
+}
