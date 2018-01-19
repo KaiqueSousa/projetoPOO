a31b56dc9383d469bdc746b5dd075dc87190d5de
KaiqueSousa <kaique.una@gmail.com>
2018-01-19 19:40:06 -0200

    Adicionando classe de modelo vendedores

---
+++ projeto-POO/src/modelos/vendedores.java
@@ -0,0 +1,67 @@
+
+package modelos;
+
+import java.time.LocalDate;
+
+public class vendedores extends funcionarios{
+    
+    private float salario;
+    private float comissao;
+    
+    public vendedores(String nome, String cpf, LocalDate datanasci, String codigo) {
+        super(nome, cpf, datanasci, codigo);
+        this.salario = salario;
+        this.comissao = comissao;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    public float getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(float comissao) {
+        this.comissao = comissao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + Float.floatToIntBits(this.salario);
+        hash = 11 * hash + Float.floatToIntBits(this.comissao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final vendedores other = (vendedores) obj;
+        if (Float.floatToIntBits(this.salario) != Float.floatToIntBits(other.salario)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.comissao) != Float.floatToIntBits(other.comissao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "vendedores{" + "salario=" + salario + ", comissao=" + comissao + '}';
+    }
+    
+}
